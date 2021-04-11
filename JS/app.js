c4aa03839b85a00971e7dd8ffe5927e6a8d7645e
Abelmajeed Alabedalraheem <81154281+abdu-zeyad@users.noreply.github.com>
2021-04-11 20:22:48 +0300

    location1

--- JS/app.js
+++ JS/app.js
@@ -1,26 +1,46 @@
-'use strict' ;
+'use strict';
+
 let working_hours =['6:00am','7:00am','8:00am','9:00am','10:00am','11:00am','12:00am','1:00pm','2:00pm','3:00pm','4:00pm','5:00pm','7:00pm'];
 
-const Seattle = {
-  min_hourly_customers:23,
-  max_hourly_customers:65,
-  average_cookies_per_customer:6.3,
-  hours:0,
-  gethour:function(){
-    this.hours = randomValue(min_hourly_customers,max_hourly_customers);
+//Seatle
+
+const seatle ={
+  minCust:23,
+  maxCust:65,
+  avgCookPerHr:6.3,
+  randNum:[],
+  numOfCookies:[],
+
+  random:function () {
+    return Math.random()*(this.maxCust-this.minCust)+this.minCust;
+  },
+
+  cookieNum:function () {
+    for (let i = 0; i < working_hours.length; i++) {
+      this.randNum[i]=this.random();
+      this.numOfCookies[i]=Math.floor(this.randNum[i]*this.avgCookPerHr);
+
+    }
   }
+
 };
-console.log(gethour);
+seatle.cookieNum();
+
+let cont=document.getElementById('container');
+let header=document.createElement('h1');
+
+cont.appendChild(header);
+header.textContent='Seatle';
 
 
-creatinghours:function(){
+let unorderedList=document.createElement('ul');
+cont.appendChild(unorderedList);
 
 
-  let unorderlist = document.createElement('ul');
-  article.appendChild(unorderlist);
-  let li = null;
-  for(let i = 0 ; i < this.working_hours.length ; i++){
-    li = document.createElement('li');
-    unorderlist.appendChild(li);
-    li.textContent = this.likes[i];
-  }
+for (let i = 0; i < working_hours.length; i++) {
+  let list=document.createElement('li');
+  unorderedList.appendChild(list);
+  list.textContent=working_hours[i]+'   : '+seatle.numOfCookies[i];
+
+}
+
