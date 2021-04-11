71f2c74dafc9c4529f8bd214ba799052df0ab109
Abelmajeed Alabedalraheem <81154281+abdu-zeyad@users.noreply.github.com>
2021-04-11 21:00:07 +0300

    main

--- JS/app.js
+++ JS/app.js
@@ -2,53 +2,56 @@
 
 let working_hours =['6:00am','7:00am','8:00am','9:00am','10:00am','11:00am','12:00am','1:00pm','2:00pm','3:00pm','4:00pm','5:00pm','7:00pm'];
 
-//Seatle
+function loc(x,y,z) {
+  const seatle ={
+    minCust:x,
+    maxCust:y,
+    avgCookPerHr:z,
+    randNum:[],
+    numOfCookies:[],
+    total:0,
 
-const seatle ={
-  minCust:23,
-  maxCust:65,
-  avgCookPerHr:6.3,
-  randNum:[],
-  numOfCookies:[],
-  total:0,
+    random:function () {
+      return Math.random()*(this.maxCust-this.minCust)+this.minCust;
+    },
 
-  random:function () {
-    return Math.random()*(this.maxCust-this.minCust)+this.minCust;
-  },
+    cookieNum:function () {
+      for (let i = 0; i < working_hours.length; i++) {
+        this.randNum[i]=this.random();
+        this.numOfCookies[i]=Math.floor(this.randNum[i]*this.avgCookPerHr);
+        this.total = this.total + this.numOfCookies[i];
 
-  cookieNum:function () {
-    for (let i = 0; i < working_hours.length; i++) {
-      this.randNum[i]=this.random();
-      this.numOfCookies[i]=Math.floor(this.randNum[i]*this.avgCookPerHr);
-      this.total = this.total + this.numOfCookies[i];
-
+      }
     }
+
+  };
+  seatle.cookieNum();
+
+  let cont=document.getElementById('container');
+  let header=document.createElement('h1');
+
+  cont.appendChild(header);
+  header.textContent='Seatle';
+
+
+  let unorderedList=document.createElement('ul');
+  cont.appendChild(unorderedList);
+
+
+  for (let i = 0; i < working_hours.length; i++) {
+    let list=document.createElement('li');
+    unorderedList.appendChild(list);
+    list.textContent=working_hours[i]+'   : '+seatle.numOfCookies[i];
+
   }
 
-};
-seatle.cookieNum();
 
-let cont=document.getElementById('container');
-let header=document.createElement('h1');
-
-cont.appendChild(header);
-header.textContent='Seatle';
-
-
-let unorderedList=document.createElement('ul');
-cont.appendChild(unorderedList);
-
-
-for (let i = 0; i < working_hours.length; i++) {
-  let list=document.createElement('li');
-  unorderedList.appendChild(list);
-  list.textContent=working_hours[i]+'   : '+seatle.numOfCookies[i];
-
+  let total= document.createElement('li');
+  unorderedList.appendChild(total);
+  total.textContent='total number is '+ seatle.total;
 }
 
 
-let total= document.createElement('li');
-unorderedList.appendChild(total);
-total.textContent='total number is '+ seatle.total;
 
 
+loc(23,65,6.3);
