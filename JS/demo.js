45ecd715ea332b9a2623def321a2a3132ed2a66c
Abelmajeed Alabedalraheem <81154281+abdu-zeyad@users.noreply.github.com>
2021-04-12 23:49:12 +0300

    s

--- JS/demo.js
+++ JS/demo.js
@@ -1,69 +1,53 @@
+'use strict';
 
-let working_hours =['6:00am','7:00am','8:00am','9:00am','10:00am','11:00am','12:00am','1:00pm','2:00pm','3:00pm','4:00pm','5:00pm','7:00pm'];
-
-
-
-
-let arrOfobjects=[];
-
-function Shop (location,minCust,maxCust,avgCookPerHr) {
-  this.location=location,
-  this.minCust=minCust,
-  this.maxCust=maxCust,
-  this.avgCookPerHr=avgCookPerHr,
-  this.randNum = [],
-  this.numOfCookies=[],
-
-  arrOfobjects.push(this);
+const working_hours =['6:00am','7:00am','8:00am','9:00am','10:00am','11:00am','12:00am','1:00pm','2:00pm','3:00pm','4:00pm','5:00pm','6:00pm','7:00pm'];
+let branchs = [];
+function Shop (branchName,minCust,maxCust,avgCookiesPercust){
+  this.branchName=branchName;
+  this.minCust=minCust;
+  this.maxCust=maxCust;
+  this.avgCookiesPercust=avgCookiesPercust;
+  this.custNumHourly=[];
+  this.cookisPerHour=[];
+  branchs.push(this);
 
 }
-Shop.prototype.randomCustomer=function(){
+Shop.prototype.randomCust=function(){
   for (let i=0;i<14;i++){
-    this.randNum.push(Math.floor(Math.random() * (this.maxCust - this.minCust + 1) + this.minCust));
+    this.custNumHourly.push(Math.floor(Math.random() * (this.maxCust - this.minCust + 1) + this.minCust));
   }
 };
-Shop.prototype.cookiesHr=function(){
+Shop.prototype.cookiesHourly=function(){
   for (let i=0;i<14;i++){
-    this.numOfCookies.push(Math.ceil(this.avgCookPerHr*this.randNum[i]));
+    this.cookisPerHour.push(Math.ceil(this.avgCookiesPercust*this.custNumHourly[i]));
   }
 };
-
 Shop.prototype.calc=function(){
-  this.randomCustomer();
-  this.cookiesHr();
-
+  this.randomCust();
+  this.cookiesHourly();
 };
-
-
-
-
 Shop.prototype.render=function(){
   let tableRow=document.createElement('tr');
   table.appendChild(tableRow);
   let tableData=document.createElement('td');
   tableRow.appendChild(tableData);
   tableData.textContent=this.branchName;
-  for(let i=0;i<this.working_hours;i++){
+  for(let i=0;i<this.cookisPerHour.length;i++){
     tableData=document.createElement('td');
     tableRow.appendChild(tableData);
     tableData.textContent=this.cookisPerHour[i];
   }
-  tableData=document.createElement('td');
-  tableRow.appendChild(tableData);
-  tableData.textContent=this.totalCookies;
 };
 
-let seattle = new Shop('Seattle',23,65,6.3,1);
-let tokyo = new Shop('Tokyo',3,24,1.2,1);
-let dubai = new Shop('Dubai',11,38,3.7,1);
-let paris = new Shop('Paris',20,38,2.3,1);
-let lima = new Shop('Lima',23,65,6.3,1);
+let seattle = new Shop('Seattle',23,65,6.3);
+let tokyo = new Shop('Tokyo',3,24,1.2);
+let dubai = new Shop('Dubai',11,38,3.7);
+let paris = new Shop('Paris',20,38,2.3);
+let lima = new Shop('Lima',23,65,6.3);
 
 
 let container = document.getElementById('container');
-let heading = document.createElement('h2');
-container.appendChild(heading);
-heading.textContent='Sales Statistics :';
+
 let table =document.createElement('table');
 container.appendChild(table);
 let tableRow=document.createElement('tr');
@@ -76,9 +60,7 @@ for(let i=0;i<working_hours.length;i++){
   tableRow.appendChild(tableHeader);
   tableHeader.textContent=working_hours[i];
 }
-tableHeader=document.createElement('th');
-tableRow.appendChild(tableHeader);
-tableHeader.textContent='Daily Location Total';
+
 
 seattle.calc();
 tokyo.calc();
@@ -90,27 +72,4 @@ tokyo.render();
 dubai.render();
 paris.render();
 lima.render();
-finalRow();
-console.log(arrOfobjects);
-
-
-
-function finalRow(){
-  let lastRow=document.createElement('tr');
-  table.appendChild(lastRow);
-  let tableData=document.createElement('td');
-  lastRow.appendChild(tableData);
-  tableData.textContent='Totals/hrs';
-  let cookiesForAll=0;
-  for(let i=0;i<working_hours.length;i++){
-    let totalAtOneHour=0;
-
-    tableData=document.createElement('td');
-    lastRow.appendChild(tableData);
-    tableData.textContent=totalAtOneHour;
-    cookiesForAll=cookiesForAll+totalAtOneHour;
-  }
-  let lastData=document.createElement('td');
-  lastRow.appendChild(lastData);
-  lastData.textContent=cookiesForAll;
-}
+console.log(branchs);
