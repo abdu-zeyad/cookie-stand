dc5718944dff725bd1b1d133733548af51f02c31
Abelmajeed Alabedalraheem <81154281+abdu-zeyad@users.noreply.github.com>
2021-04-14 15:23:32 +0300

    finish lab07

--- JS/app.js
+++ JS/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-let working_hours =['6:00am','7:00am','8:00am','9:00am','10:00am','11:00am','12:00am','1:00pm','2:00pm','3:00pm','4:00pm','5:00pm','7:00pm'];
+let hours =['6:00am','7:00am','8:00am','9:00am','10:00am','11:00am','12:00am','1:00pm','2:00pm','3:00pm','4:00pm','5:00pm','6:00pm','7:00pm'];
 let locationsa =['seatle','tokyo','Dubai','Paris','Lima'];
 let min =[23,3,11,20,2];
 let max =[65,24,38,38,16];
@@ -21,7 +21,7 @@ function loc(x,y,z,b) {
     },
 
     cookieNum:function () {
-      for (let i = 0; i < working_hours.length; i++) {
+      for (let i = 0; i < hours.length; i++) {
         this.randNum[i]=this.random();
         this.numOfCookies[i]=Math.floor(this.randNum[i]*this.avgCookPerHr);
         this.total = this.total + this.numOfCookies[i];
@@ -43,10 +43,10 @@ function loc(x,y,z,b) {
   cont.appendChild(unorderedList);
 
 
-  for (let i = 0; i < working_hours.length; i++) {
+  for (let i = 0; i < hours.length; i++) {
     let list=document.createElement('li');
     unorderedList.appendChild(list);
-    list.textContent=working_hours[i]+'   : '+seatle.numOfCookies[i];
+    list.textContent=hours[i]+'   : '+seatle.numOfCookies[i];
 
   }
 
--- JS/demo.js
+++ JS/demo.js
@@ -1,75 +1,50 @@
-'use strict';
-
-const working_hours =['6:00am','7:00am','8:00am','9:00am','10:00am','11:00am','12:00am','1:00pm','2:00pm','3:00pm','4:00pm','5:00pm','6:00pm','7:00pm'];
-let branchs = [];
-function Shop (branchName,minCust,maxCust,avgCookiesPercust){
-  this.branchName=branchName;
-  this.minCust=minCust;
-  this.maxCust=maxCust;
-  this.avgCookiesPercust=avgCookiesPercust;
-  this.custNumHourly=[];
-  this.cookisPerHour=[];
-  branchs.push(this);
-
-}
-Shop.prototype.randomCust=function(){
-  for (let i=0;i<14;i++){
-    this.custNumHourly.push(Math.floor(Math.random() * (this.maxCust - this.minCust + 1) + this.minCust));
-  }
-};
-Shop.prototype.cookiesHourly=function(){
-  for (let i=0;i<14;i++){
-    this.cookisPerHour.push(Math.ceil(this.avgCookiesPercust*this.custNumHourly[i]));
-  }
-};
-Shop.prototype.calc=function(){
-  this.randomCust();
-  this.cookiesHourly();
-};
-Shop.prototype.render=function(){
-  let tableRow=document.createElement('tr');
-  table.appendChild(tableRow);
-  let tableData=document.createElement('td');
-  tableRow.appendChild(tableData);
-  tableData.textContent=this.branchName;
-  for(let i=0;i<this.cookisPerHour.length;i++){
-    tableData=document.createElement('td');
-    tableRow.appendChild(tableData);
-    tableData.textContent=this.cookisPerHour[i];
-  }
-};
-
-let seattle = new Shop('Seattle',23,65,6.3);
-let tokyo = new Shop('Tokyo',3,24,1.2);
-let dubai = new Shop('Dubai',11,38,3.7);
-let paris = new Shop('Paris',20,38,2.3);
-let lima = new Shop('Lima',23,65,6.3);
 
 
-let container = document.getElementById('container');
 
-let table =document.createElement('table');
-container.appendChild(table);
-let tableRow=document.createElement('tr');
-table.appendChild(tableRow);
-let tableHeader=document.createElement('th');
-tableRow.appendChild(tableHeader);
-tableHeader.textContent='      ';
-for(let i=0;i<working_hours.length;i++){
-  tableHeader=document.createElement('th');
-  tableRow.appendChild(tableHeader);
-  tableHeader.textContent=working_hours[i];
-}
+// // get the form
+// const form = document.getElementById('kittenForm');
+
+// // second i need to attach it to a addeventlistener!
+
+// form.addEventListener('submit', handleSubmitting);
+
+// function handleSubmitting(event){
+//   event.preventDefault(); // will prevent refreshing when you submit!
+//   // console.log('Event of the form! :',event);
+//   // console.log(event.target.nameField.value);
+//   // console.log(event.target.likesField.value);
+//   console.log(event);
+//   let newkittenName =event.target.nameField.value;
+//   // console.log(newkittenName);
+//   let likes = event.target.likesField.value;
+//   // console.log(typeof likes);
+//   // we need to change the data type from a string to an array
+//   console.log(likes);
+//   likes = likes.split(',');
+//   console.log(likes);
+//   let breed = event.target.breedField.value;
+//   // console.log(breed);
+//   let dogs = event.target.isGoodWithDogs.checked;
+//   // console.log(dogs);
+//   let kids = event.target.isGoodWithKids.checked;
+
+//   let cats  = event.target.isGoodWithOtherCats.checked;
+
+//   let newKitten = new Kitten(newkittenName,likes,breed,kids,cats,dogs);
 
 
-seattle.calc();
-tokyo.calc();
-dubai.calc();
-paris.calc();
-lima.calc();
-seattle.render();
-tokyo.render();
-dubai.render();
-paris.render();
-lima.render();
-console.log(branchs);
+//   newKitten.getAge();
+//   newKitten.render();
+
+// }
+
+
+
+
+
+
+
+// // for(let i = 0 ; i< arrOfobjects.length; i++){
+// //   arrOfobjects[i].getAge();
+// //   arrOfobjects[i].render();
+// // }
