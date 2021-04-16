784451e34408be192adcad9c9d574ba37581fc58
abdalmajeed-zeyad <abd.d@live.com>
2021-04-16 14:37:50 +0300

    a

--- JS/app.js
+++ JS/app.js
@@ -1,9 +1,41 @@
 'use strict';
 
 let container = document.getElementById('container');
-
 let table =document.createElement('table');
 container.appendChild(table);
+
+
+const hours =['6:00am','7:00am','8:00am','9:00am','10:00am','11:00am','12:00am','1:00pm','2:00pm','3:00pm','4:00pm','5:00pm','6:00pm','7:00pm'];
+let branchs = [];
+console.log(branchs);
+function Shop (location,minCust,maxCust,avgCookiesPercust){
+  this.Location=location;
+  this.minCust=minCust;
+  this.maxCust=maxCust;
+  this.avgCookiesPercust=avgCookiesPercust;
+  this.custNumHourly=[];
+  this.cookisPerHour=[];
+  this.totalCookies=0;
+  branchs.push(this);
+
+}
+
+
+Shop.prototype.randomCust_f=function(){
+  for (let i=0;i<hours.length;i++){
+    this.custNumHourly.push(Math.floor(Math.random() * (this.maxCust - this.minCust + 1) + this.minCust));
+  }
+};
+
+
+Shop.prototype.cookiesHourly_f=function(){
+  for (let i=0;i<hours.length;i++){
+    this.cookisPerHour.push(Math.ceil(this.avgCookiesPercust*this.custNumHourly[i]));
+    this.totalCookies=this.totalCookies+this.cookisPerHour[i];
+
+  }
+
+};
 
 
 function headerRow(){
@@ -24,38 +56,11 @@ function headerRow(){
   dailyhours.textContent=('daily hour');
 
 }
+headerRow();
 
-const hours =['6:00am','7:00am','8:00am','9:00am','10:00am','11:00am','12:00am','1:00pm','2:00pm','3:00pm','4:00pm','5:00pm','6:00pm','7:00pm'];
-let branchs = [];
-console.log(branchs);
-function Shop (location,minCust,maxCust,avgCookiesPercust){
-  this.Location=location;
-  this.minCust=minCust;
-  this.maxCust=maxCust;
-  this.avgCookiesPercust=avgCookiesPercust;
-  this.custNumHourly=[];
-  this.cookisPerHour=[];
-  this.totalCookies=0;
-  branchs.push(this);
 
-}
-// get random number of customer and push it to the first array.
-Shop.prototype.randomCust_f=function(){
-  for (let i=0;i<hours.length;i++){
-    this.custNumHourly.push(Math.floor(Math.random() * (this.maxCust - this.minCust + 1) + this.minCust));
-  }
-};
-//  this the number of the cookies per hour( the customer * avg ) and the total number per a day.
-// push into array cookies per hour and in value total cookies
-Shop.prototype.cookiesHourly_f=function(){
-  for (let i=0;i<hours.length;i++){
-    this.cookisPerHour.push(Math.ceil(this.avgCookiesPercust*this.custNumHourly[i]));
-    this.totalCookies=this.totalCookies+this.cookisPerHour[i];
 
-  }
 
-};
-// this function is used to display the table headers on html which is locations (th), and cells which is td.(creat a colum)
 Shop.prototype.render_f=function(){
 
   let rawData = document.createElement('tr');
@@ -73,14 +78,12 @@ Shop.prototype.render_f=function(){
   rawData.appendChild(cell);
   cell.textContent = this.totalCookies;
 };
-// this function is just for help and collecting and excecuting other functions.
+
+
 Shop.prototype.calc=function(){
   this.randomCust_f();
   this.cookiesHourly_f();
-  this.render_f();
-
-};
-// this function is used to creat the first row which is the hours.
+  this.render_f();}
 
 
 let seattle = new Shop('Seattle',23,65,6.3);
@@ -88,17 +91,11 @@ let tokyo = new Shop('Tokyo',3,24,1.2);
 let dubai = new Shop('Dubai',11,38,3.7);
 let paris = new Shop('Paris',20,38,2.3);
 let lima = new Shop('Lima',23,65,6.3);
-
-headerRow();
-
-
 seattle.calc();
 tokyo.calc();
 dubai.calc();
 paris.calc();
 lima.calc();
-
-
 
 
 function final(){
@@ -126,16 +123,8 @@ final();
 
 
 
-
-
-
-
-
-
 const form = document.getElementById('newbranchform');
-
 form.addEventListener('submit', addlocation);
-
 function addlocation(event){
   event.preventDefault ();
 
@@ -146,17 +135,29 @@ function addlocation(event){
 
   let newlocation = new Shop (nlocation,min,max,avg);
   newlocation.calc();
-  // String.split; // to change from string to array
-  // breed =parseFloat(breed);
-  // .
-  // .
-  // .
-  // .
-  // .
-    document.getElementById('table').deleteRow(-1);
-  
-  createTable();
+  var rowCount = table.rows.length;
+  table.deleteRow(rowCount -2);
+  final();
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// this function is just for help and collecting and excecuting other functions.
+
 // function createTable() {
   
   // renderTheadTable();
